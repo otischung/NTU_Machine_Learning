8b57c9a04d9e1d6277e207a83dd9ecd1a14ff544
otischung <my072814638@gmail.com>
2022-01-28 00:10:33 +0800

    use MEGA to download instead; add rm .zip script into script

--- MLHW03/Download_Data.sh
+++ MLHW03/Download_Data.sh
@@ -2,15 +2,16 @@
 # You may choose where to download the data.
 
 # Google Drive
-gdown --id '1awF7pZ9Dz7X1jn1_QAiKN-_v56veCEKy' --output food-11.zip
+# gdown --id '1awF7pZ9Dz7X1jn1_QAiKN-_v56veCEKy' --output food-11.zip
 
 # Dropbox
-# !wget https://www.dropbox.com/s/m9q6273jl3djall/food-11.zip -O food-11.zip
+# wget https://www.dropbox.com/s/m9q6273jl3djall/food-11.zip -O food-11.zip
 
 # MEGA
-# !sudo apt install megatools
-# !megadl "https://mega.nz/#!zt1TTIhK!ZuMbg5ZjGWzWX1I6nEUbfjMZgCmAgeqJlwDkqdIryfg"
+# sudo apt install megatools
+megadl "https://mega.nz/#!zt1TTIhK!ZuMbg5ZjGWzWX1I6nEUbfjMZgCmAgeqJlwDkqdIryfg"
 
 # Unzip the dataset.
 # This may take some time.
 unzip -q food-11.zip
+rm food-11.zip
